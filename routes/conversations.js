8c8ca0477ca0de0f909da0639324f16934c45139
CoderBig <49120922+dogukanc760@users.noreply.github.com>
2022-02-11 16:43:26 +0300

    Update conversations.js

--- routes/conversations.js
+++ routes/conversations.js
@@ -6,6 +6,7 @@ const Conversation = require("../models/Conversation");
 router.post("/", async (req, res) => {
   const newConversation = new Conversation({
     members: [req.body.senderId, req.body.receiverId],
+    targetName: req.body.targetName
   });
 
   try {
